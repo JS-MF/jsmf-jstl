f650c687f79da27f70ac643ccfb39959efd29546
Nicolas Biri <nicolas.biri@list.lu>
2016-05-11 14:16:58 +0200

    ES2015 migration

--- index.js
+++ index.js
@@ -13,22 +13,20 @@ Contributors: G. Garcia-Frey
             - Multiple input/output models
             - Hide the complex object for input
 */
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-var JSMF = require('jsmf-core');
+const _ = require('lodash')
+const JSMF = require('jsmf-core')
 
 
 /** @constuctor
  * Initiate a transformation module.
  */
 function Transformation(rules, helpers) {
-    var rs = [];
-    _.forEach(rules, function(x) {rs.push(x);});
-    this.rules = rs;
-    var hs = [];
-    _.forEach(helpers, function(x) {hs.push(x);});
-    this.helpers = hs;
+    this.rules = []
+    this.helpers = []
+    _.forEach(rules, x => this.addRule(x))
+    _.forEach(helpers, x => this.addHelper(x))
 }
 
 /** Add a transformation rule to a transformation module.
@@ -40,12 +38,12 @@ function Transformation(rules, helpers) {
  *  @param {string} r.name - An optional name for the rule, not used yet.
  */
 Transformation.prototype.addRule = function(r) {
-    if (_.every(['in', 'out'], function(x) {return _.has(r, x);})) {
-        this.rules.push(r);
+    if (_.every(['in', 'out'], x => _.has(r, x))) {
+        this.rules.push(r)
     } else {
-        throw new Error('Invalid rule:' + r);
+        throw new Error(`Invalid rule: ${r}`)
     }
-};
+}
 
 
 /** Add a helper to a transformation module.
@@ -54,24 +52,24 @@ Transformation.prototype.addRule = function(r) {
  *  @param {string} h.name - the name of the helper, that will be used to reference the helper when we need it.
  */
 Transformation.prototype.addHelper = function(h) {
-    if (_.every(['map', 'name'], function(x) {return _.has(h, x);})) {
-        this.helpers.push(h);
+    if (_.every(['map', 'name'], x => _.has(h, x))) {
+        this.helpers.push(h)
     } else {
-        throw new Error('Invalid helper:' + h);
+        throw new Error(`Invalid helper: ${h}`)
     }
-};
-
-function Context() {
-    this.generated = new Mapping();
-    this.helpers = {};
-    this.referencesResolutions = [];
-    this.generationLog = new Mapping();
 }
 
-Context.prototype.addResolution = function(r) {this.referencesResolutions.push(r);};
+function Context() {
+    this.generated = new Map()
+    this.helpers = {}
+    this.referencesResolutions = []
+    this.generationLog = new Map()
+}
+
+Context.prototype.addResolution = function(r) {this.referencesResolutions.push(r)}
 
 Context.prototype.assign = function(element, relationName, populators) {
-    this.addResolution(new ReferenceResolution(element, relationName, populators));
+    this.addResolution(new ReferenceResolution(element, relationName, populators))
 }
 
 /** @constructor
@@ -84,27 +82,28 @@ Context.prototype.assign = function(element, relationName, populators) {
  */
 function Rule(selection, out, name) {
     /** @member {function} in */
-    this.in = selection;
+    this.in = selection
     /** @member {function} out */
-    this.out = out;
+    this.out = out
     /** @member {string} name */
-    this.name = name;
+    this.name = name
 }
 
 function runRule(rule, context, inputModel, outputModel, debug) {
-    var selection = rule.in.call(context, inputModel);
+    var selection = rule.in.call(context, inputModel)
     _.forEach(selection, function(e) {
-        var generated = rule.out.call(context, e, inputModel);
-        context.generated.map(e, generated);
+        var generated = rule.out.call(context, e, inputModel)
+        const value = (context.generated.get(e) || [])
+        context.generated.set(e, value.concat(generated))
         if (debug) {
           _.forEach(generated, function(x) {
-              context.generationLog.map(x, {rule: rule, source: e});
-          });
+              const logValue = context.generationLog.get(e) || []
+              logValue.push({rule, source: e})
+              context.generationLog.map(x, logValue)
+          })
         }
-        _.forEach(generated, function(x) {
-            outputModel.addModellingElement(x);
-        });
-    });
+        _.forEach(generated, x => outputModel.addModellingElement(x))
+    })
 }
 
 /** @constructor
@@ -112,13 +111,13 @@ function runRule(rule, context, inputModel, outputModel, debug) {
  *  @param {string} name - the name of the helper, that will be used to reference the helper when we need it.
  */
 function Helper(generation, name) {
-    this.map = generation;
-    this.name = name;
+    this.map = generation
+    this.name = name
 }
 
 function runHelper(helper, context, inputModel, outputModel) {
-    var generated = helper.map.call(context, inputModel);
-    context.helpers[helper.name] = generated;
+    var generated = helper.map.call(context, inputModel)
+    context.helpers[helper.name] = generated
 }
 
 /** @constructor
@@ -127,23 +126,23 @@ function runHelper(helper, context, inputModel, outputModel) {
  * @param {Object[]} populators - the input model elements that should be resolved to populate the relation.
  */
 function ReferenceResolution(element, relationName, populators) {
-    this.source = element;
-    this.relationName = relationName;
-    this.target = populators;
+    this.source = element
+    this.relationName = relationName
+    this.target = populators
 }
 
 function runResolution(resolution, generated) {
-    var relationType = resolution.source.conformsTo().getAllReferences()[resolution.relationName].type;
-    var referenceFunctionName = 'add' + resolution.relationName[0].toUpperCase() + resolution.relationName.slice(1);
+    const relationType = resolution.source.conformsTo().getAllReferences()[resolution.relationName].type
+    const referenceFunctionName = 'add' + resolution.relationName[0].toUpperCase() + resolution.relationName.slice(1)
     _.each(resolution.target,  // get the type of the target(s) of the relation element in the input model in order to...
            function(elem) {
-               var values = generated.valuesFor(elem) || [];
-               _.each(values, function(target) {
+               const values = generated.get(elem) || []
+               _.each(values, target => {
                     if (relationType === JSMF.JSMFAny || relationType === undefined || hasClass(target, relationType)) {
-                        resolution.source[referenceFunctionName](target);
+                        resolution.source[referenceFunctionName](target)
                     }
-               });
-           });
+               })
+           })
 }
 
 /** Apply a trsnformation on an input model, and put generated elements in a given ouput model.
@@ -152,62 +151,17 @@ function runResolution(resolution, generated) {
  * @param {boolean} debug - Store additional information during the transformation.
  */
 Transformation.prototype.apply = function(inputModel, outputModel, debug) {
-    var ctx = new Context();
-    outputModel = outputModel || new JSMF.Model('TransformationOutput');
-    _.forEach(this.helpers, function(h) {
-        runHelper(h, ctx, inputModel, outputModel);
-    });
-    _.forEach(this.rules, function(r) {
-        runRule(r, ctx, inputModel, outputModel, debug);
-    });
-    _.forEach(ctx.referencesResolutions, function(r) {
-        runResolution(r, ctx.generated);
-    });
-    return ctx;
+    const ctx = new Context()
+    outputModel = outputModel || new JSMF.Model('TransformationOutput')
+    _.forEach(this.helpers, h => runHelper(h, ctx, inputModel, outputModel))
+    _.forEach(this.rules, r => runRule(r, ctx, inputModel, outputModel, debug))
+    _.forEach(ctx.referencesResolutions, r => runResolution(r, ctx.generated))
+    return ctx
 }
 
 var hasClass = function (x, type) {
-    var types = type instanceof Array ? type : [type];
-    return _.some(x.conformsTo().getInheritanceChain(),
-                  function (c) {return _.includes(types, c)});
+    const types = type instanceof Array ? type : [type]
+    return _.some(x.conformsTo().getInheritanceChain(), c => _.includes(types, c))
 }
 
-function Mapping() {
-    // aberration, we should use ES6 Map ASAP
-}
-
-Mapping.prototype.findEntry = function(k) {
-    return _.find(this[JSMF.jsmfId(k)], function(x) {return x.key === k;});
-}
-
-Mapping.prototype.valuesFor = function(k) {
-    var entry = this.findEntry(k);
-    if (entry !== undefined) {return entry.values;}
-}
-
-Mapping.prototype.map = function(k, v) {
-    var key = JSMF.jsmfId(k);
-    if (key === undefined) {
-        throw new Error('Invalid key for mapping: ' + k);
-    }
-    var entry = this.findEntry(k);
-    if (entry === undefined) {
-        entry = {key: k, values: []};
-        if (this[key] === undefined) {
-            this[key] = [entry];
-        } else {
-            this[key].push(entry);
-        }
-    }
-    v = v instanceof Array ? v : [v];
-    entry.values = entry.values.concat(v);
-}
-
-module.exports = {
-
-    Mapping: Mapping,
-    Helper: Helper,
-    Rule: Rule,
-    Transformation: Transformation
-
-};
+module.exports = {Helper, Rule, Transformation}
